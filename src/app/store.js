e5ccdf74427fc6ea1ea66e9dd96ddd6bf5a5882a
unlomtrois <vadim.kuz02@gmail.com>
2021-02-12 13:40:34 +0300

    remove initial counter

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import notesReducer from '../features/notes/notesSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     notes: notesReducer
   },
 });
